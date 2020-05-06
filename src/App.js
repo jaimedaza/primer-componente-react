b40acf6f2abb133e4995eb798a5c54fd73915872
Jaime Daza <jaimedaza11@gmail.com>
2020-05-05 19:15:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import Welcome from "./Welcome"; // importarlo
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      names: ["Juan", "Pedro", "Germán"]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.names.map((name) =>
+          <Welcome name={name} key={name}/>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
